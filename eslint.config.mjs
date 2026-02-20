974f3884a9f7dbe02d355a688f32d67cdce30b4c
Dann-13 <dannyelgomezz.13@gmail.com>
2026-02-19 20:53:39 -0500

    feat(up): first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist/**', 'node_modules/**', 'commitlint.config.cjs', 'eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+);
